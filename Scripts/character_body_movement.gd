7e6ae9661e4102a810e5aa5bb0faa05efa42ee85
CupChad <cupchadofficial@gmail.com>
2026-01-04 02:42:18 -0500

    Character, Objects, and Area Changes
    Changed character movement to be appropriate, changed area to be enclosed and flat instead of a platform. Changed player and objects to be new scenes, allowing them to be added freely.

---
+++ Scripts/character_body_movement.gd
@@ -0,0 +1,11 @@
+extends CharacterBody2D
+
+# @export is what makes this variable visible in the editor.
+# --CANNOT-- be put into a function.
+@export var speed: float = 200.0
+
+func _physics_process(_delta: float) -> void:
+	var direction = Input.get_vector("move_left", "move_right", "move_up", "move_down")
+	# Velocity is built in to CharacterBody2D
+	velocity = direction * speed
+	move_and_slide()
