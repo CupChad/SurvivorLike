cbd87feb7111a9e0e967a3daef760d3b738dec86
CupChad <cupchadofficial@gmail.com>
2026-01-05 05:59:16 -0500

    Changed enemy AI, added non functional healthbar.

--- Scripts/enemyMovement.gd
+++ Scripts/enemyMovement.gd
@@ -3,12 +3,11 @@ extends CharacterBody2D
 
 @export var speed: float = 200.0
 
-func _process(delta: float) -> void:
+@onready var player = get_node("/root/main/character/Player")
+
+func _physics_process(_delta):
 	# Gets player position.
-	var player = get_tree().get_first_node_in_group("player")
-	if not player:
-		return
-	var destination = player.position
-	# Moves enemy toward player.
-	position = position.move_toward(destination, speed * delta)
-	
+	var direction = global_position.direction_to(player.global_position)
+	# Moves enemy towards player.
+	velocity = direction * speed
+	move_and_slide()
